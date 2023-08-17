87cbf52cf3d15d6aba9caa075ecbcb811e58ba74
kirillparfenov <toonart.ru@gmail.com>
2023-08-17 21:29:47 +0300

    4. Базовые структуры алгоритмов

--- src/main/java/dev/manrihter/lesson_4_базовые_структуры_данных/Main.java
+++ src/main/java/dev/manrihter/lesson_4_базовые_структуры_данных/Main.java
@@ -5,6 +5,9 @@ import dev.manrihter.lesson_4_базовые_структуры_данных.mod
 import dev.manrihter.lesson_4_базовые_структуры_данных.models.SingleArray;
 import dev.manrihter.lesson_4_базовые_структуры_данных.models.VectorArray;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.print("Single array 10_000:\n");
@@ -18,6 +21,10 @@ public class Main {
         System.out.print("Factor array 10_000:\n");
         BaseArray<String, String> factorArray = new FactorArray<>();
         addAndDelete(factorArray, 10_000);
+
+        System.out.print("ArrayList 10_000:\n");
+        List<String> list = new ArrayList<>();
+        addAndDeleteArrayList(list, 10_000);
 
         System.out.println();
     }
@@ -33,6 +40,22 @@ public class Main {
         //delete -  первый элемент в массиве
         startMillis = System.currentTimeMillis();
         for (int i = 0; i < total; i++) array.deleteByIndex(0);
+        System.out.println(
+                String.format("Total time for delete: %s ms\n", System.currentTimeMillis() - startMillis)
+        );
+    }
+
+    private static void addAndDeleteArrayList(List<String> list, int total) {
+        //add
+        var startMillis = System.currentTimeMillis();
+        for (int i = 0; i < total; i++) list.add("" + i);
+        System.out.println(
+                String.format("Total time for add: %s ms", System.currentTimeMillis() - startMillis)
+        );
+
+        //delete -  первый элемент в массиве
+        startMillis = System.currentTimeMillis();
+        for (int i = 0; i < total; i++) list.remove("" + i);
         System.out.println(
                 String.format("Total time for delete: %s ms\n", System.currentTimeMillis() - startMillis)
         );
